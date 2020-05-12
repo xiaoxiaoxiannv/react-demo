c80dba90a32735492d2ead81b210ff7f77b98d12
Luna <1017929760@qq.com>
2020-05-12 18:52:08 +0800

    添加state（内部数据）

--- src/index.js
+++ src/index.js
@@ -7,29 +7,43 @@ function App() {
     return(
         <div className="App">
             爸爸
-            <Son messageForSon="儿子你好"/>
+            <Son />
         </div>
     )
 }
 
 class Son extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            n: 0
+        };
+    }
+    add() {
+        this.setState((state)=>{
+            return {n:state.n +1}
+        })//setState是异步更新ui的过程
+    }
     render(){
         return(
             <div className="Son">
-                我是儿子，我爸对我说「{this.props.messageForSon}」
-                <Grandson messageForGrandson="孙子你好"/>
+                儿子 n: {this.state.n}
+                <button onClick={() => this.add()}>+1</button>
+                <Grandson />
             </div>
         );
     }
 }
 
 const Grandson = (props)=>{
-    return(
+    const [n, setN] = React.useState(0);
+    return (
         <div className="Grandson">
-            我是孙子，我爸对我说「{props.messageForGrandson}」
+            孙子 n:{n}
+            <button onClick={() => setN(n + 1)}>+1</button>
         </div>
-    )
-}
+    );
+};
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
